a67c8b5b8d8abd2b1a0315b61a8ab9e47166be0c
U201620280 <43019447+JALG99@users.noreply.github.com>
2021-05-03 12:29:40 -0500

    Finalización de la Inspección y comentarios

--- ta1_script.R
+++ ta1_script.R
@@ -1,9 +1,27 @@
+#CARGAR DATOS
 hotel.booking = read.csv("./Datos/hotel_bookings.csv", header = TRUE, sep = ",")
 
+#INSPECCIONAR DATOS
 View(hotel.booking)
 
 head(hotel.booking, 5)
 
+tail(hotel.booking, 5)
+
 names(hotel.booking)
 
+str(hotel.booking)
 
+summary(hotel.booking)
+
+hotel.booking$is_canceled = as.factor(hotel.booking$is_canceled)
+hotel.booking$arrival_date_year = as.factor(hotel.booking$arrival_date_year)
+hotel.booking$arrival_date_week_number = as.factor(hotel.booking$arrival_date_week_number)
+hotel.booking$arrival_date_day_of_month = as.factor(hotel.booking$arrival_date_day_of_month)
+hotel.booking$is_repeated_guest = as.factor(hotel.booking$is_repeated_guest)
+
+str(hotel.booking)
+
+summary(hotel.booking)
+
+#LIMPIAR DATOS
