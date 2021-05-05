459c0be1591bbfc077bc749e99a6808e0ac7fe22
U201620280 <43019447+JALG99@users.noreply.github.com>
2021-05-05 16:17:16 -0500

    Primera gráficas

--- ta1_script.R
+++ ta1_script.R
@@ -11,8 +11,11 @@ tail(hotel.booking, 5)
 names(hotel.booking)
 
 str(hotel.booking)
+#Al revisar los datos nos damos cuenta que existe múltiples valores que funcionarían mejor como factor.
 
 summary(hotel.booking)
+#Con Summary corroboramos que hay datos numércios que estan cumpliendo la función de factor y deben ser corregidos, además
+#se nota la presencia de algunos NAs en Children, sin embargo la limpieza necesaria se hará luego.
 
 hotel.booking$is_canceled = as.factor(hotel.booking$is_canceled)
 hotel.booking$arrival_date_year = as.factor(hotel.booking$arrival_date_year)
@@ -23,5 +26,17 @@ hotel.booking$is_repeated_guest = as.factor(hotel.booking$is_repeated_guest)
 str(hotel.booking)
 
 summary(hotel.booking)
+#Finalmente comprobamos que los datos están correctos en cuanto al tiempo y podemos proceder con el análisis y selección
+#de visualizaciones.
 
-#LIMPIAR DATOS
+#VISUALIZACIONES
+counts = table(hotel.booking$customer_type, hotel.booking$reserved_room_type)
+barplot(counts, col=c("green","yellow","blue","red"), legend = c("Contrato", "Grupo", "Transitorio", "Transitorio asociado"), main = "Tipo de cliente por habitación reservada")
+#Esta gráfica nos ayuda a verificar dos cosas, primero cuales son los tipos de habitación más reservados en relación con el tipo de cliente, vemos que en todos los tipos de habitación
+#predomina el transitorio, además nos permite conocer los tipos de habitación más solicitadas en las reservas, notando una clara victoria por parte del tipo A seguido del tipo D.
+
+counts2 = table(hotel.booking$reservation_status, hotel.booking$deposit_type)
+barplot(counts2, col=c("blue","red","green"), legend = c("Cancelado", "Check-out", "No se muestra"), main = "Estado de la reservación por el tipo de deposito")
+#Esta gráfica nos ayuda a revisar por el estado de la reservación que tipo de deposito predomina y notamos algo increible, en el caso de los que no hacen ningún depósito la gran mayoría
+#termina completando su estadía y retirandose, sin embargo en los realizan un depósito completo que no tiene lugar a devoluciones se ve que predomina por mucho la cantidad de clientes
+#que cancelaron.
