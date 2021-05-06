07265b6024775adbc65f0d18532eb59c21361028
U201620280 <43019447+JALG99@users.noreply.github.com>
2021-05-05 22:32:02 -0500

    Organización

--- Scripts/ta1_script.R
+++ Scripts/ta1_script.R
@@ -27,6 +27,13 @@ hotel.booking$arrival_date_year = as.factor(hotel.booking$arrival_date_year)
 hotel.booking$arrival_date_week_number = as.factor(hotel.booking$arrival_date_week_number)
 hotel.booking$arrival_date_day_of_month = as.factor(hotel.booking$arrival_date_day_of_month)
 hotel.booking$is_repeated_guest = as.factor(hotel.booking$is_repeated_guest)
+#Se convirtieron estas variables a Factor y se realizó una verificación posterior
+
+str(hotel.booking)
+
+summary(hotel.booking)
+#Finalmente comprobamos que los datos están correctos y podemos proceder con el análisis y selección
+#de visualizaciones.
 
 str(hotel.booking)
 
